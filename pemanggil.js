d8512ad2f88a9657fce68e490daedd30100e1d39
donaldjack <39451962+donaldjack@users.noreply.github.com>
2018-12-11 13:30:50 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -19,10 +19,11 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*2)+ 1);
+var x=Math.floor((Math.random()*3)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2018/12/choosing-windows-web-hosting.html"}
-if(x=="2"){xxx="/2018/12/web-server-hosting-types.html"}a_to_ve[i].href="https://www.kangsense.pw"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="/2018/12/web-server-hosting-types.html"}
+if(x=="3"){xxx="/2018/12/do-you-need-lot-of-bandwidth.html"}a_to_ve[i].href="https://www.kangsense.pw"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
